d8538863282595244c6f04be537d8a3366918c3a
Shuaib Hussein <shuaibh97@gmail.com>
2022-01-30 17:22:08 +0000

    setup three layer classes

--- app/src/main/java/com/qa/app/service/PlayerService.java
+++ app/src/main/java/com/qa/app/service/PlayerService.java
@@ -1,5 +1,63 @@
 package com.qa.app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.app.data.entity.Player;
+import com.qa.app.data.repository.PlayerRepository;
+
+
+@Service
 public class PlayerService {
 
+	
+	
+	private final PlayerRepository playerRepository;
+	
+
+	@Autowired
+	public PlayerService(PlayerRepository playerRepository) {
+		super();
+		this.playerRepository = playerRepository;
+	}
+
+	
+
+//	List<Player> test = List.of(new Player(1L, "Shuaib", "Hussein", "Midfielder", 0, 0),
+//			   new Player(2L, "Abdi", "Hussein", "Attacker", 0, 0));
+
+
+	public List<Player> getPlayers() {
+		
+		return playerRepository.findAll();
+	}
+	
+	public Player getById(Long id) {
+		return null;
+		
+	}
+	
+	
+	public Player create(Player player) {
+		
+		return null;
+		
+	}
+	
+	public Player update(Long id, Player player) {
+		
+		return null;
+	}
+	
+	
+	public void delete(long id) {
+		
+	}
+
+
+	
+	
+	
 }
